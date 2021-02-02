5d111458f51b5945e53507cdce7e90caddf91e5c
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-02 20:50:04 +0000

    Assert Equal for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }    
+  }
+  return true;
+}
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
